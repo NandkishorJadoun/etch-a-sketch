84066b7527a7d1fd7a743a67ad70959ff84b7e07
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2024-12-17 15:54:06 +0530

    add event to the reset button

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const container = document.querySelector("#container");
+const reset = document.querySelector(".reset")
 
-function makeGrid(size){
+
+function makeGrid(size = 16){
     for(i=0;i<size;i++){
         let row = document.createElement("div")
-        row.setAttribute("class", "row-div")
 
         for(j=0; j<size;j++){
             let column = document.createElement("div")
-            column.setAttribute("class", "column-div")
+            column.setAttribute("class", "grid-box-before")
             row.appendChild(column)
+
+            column.addEventListener("mouseout", ()=>{
+                column.setAttribute("class", "grid-box-after")
+            })
+
+            reset.addEventListener("click" , ()=>{
+                column.setAttribute("class", "grid-box-before")
+            })
         }
         container.appendChild(row)
     }

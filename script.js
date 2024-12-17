7944b4b754c14b033498c159cfa5154b8dff377e
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2024-12-17 14:29:31 +0530

    create 16x16 square grid

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const container = document.querySelector("#container");
 
+function makeGrid(size){
+    for(i=0;i<size;i++){
+        let row = document.createElement("div")
+        row.setAttribute("class", "row-div")
 
+        for(j=0; j<size;j++){
+            let column = document.createElement("div")
+            column.setAttribute("class", "column-div")
+            row.appendChild(column)
+        }
+        container.appendChild(row)
+    }
+}
+
+makeGrid(16);

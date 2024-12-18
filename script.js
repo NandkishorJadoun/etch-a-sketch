4c2b2612b76704b2ea1fa1ec63884d26d08dc477
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2024-12-18 08:59:38 +0530

    add change-size button logic

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 const container = document.querySelector("#container");
-const reset = document.querySelector(".reset")
-
+const reset = document.querySelector(".reset");
+const changeSizeButton = document.querySelector(".change-size");
 
 function makeGrid(size = 16){
     for(i=0;i<size;i++){
         let row = document.createElement("div");
-        row.setAttribute("class", "row-div")
+        row.setAttribute("class", "row-div");
 
         for(j=0; j<size;j++){
-            let column = document.createElement("div")
-            column.setAttribute("class", "grid-box-before")
+            let column = document.createElement("div");
+            column.setAttribute("class", "grid-box-before");
             row.appendChild(column)
 
             column.addEventListener("mouseout", ()=>{
-                column.setAttribute("class", "grid-box-after")
+                column.setAttribute("class", "grid-box-after");
             })
 
             reset.addEventListener("click" , ()=>{
-                column.setAttribute("class", "grid-box-before")
+                column.setAttribute("class", "grid-box-before");
             })
         }
         container.appendChild(row)
     }
 }
 
-makeGrid(16);
+changeSizeButton.addEventListener("click", ()=>{
+    let newSize = prompt("Choose from 1 to 100", "");
+    if(newSize == undefined||newSize == null || newSize == ''){
+        return;
+    }
+    else if(newSize>100 || newSize<0){
+        alert("That's not between 1 and 100");
+    }
+    else{
+        makeGrid(newSize);
+    }
+})
+
+makeGrid();
